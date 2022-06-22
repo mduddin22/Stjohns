ebb7193b34f51a155daf3103880cdde9c68b2344
mduddin15 <rakibuddin.md@gmail.com>
2022-06-22 19:17:46 -0400

    Simple Array

---
+++ MyArray/src/arrayinput/ArrayPractice.java
@@ -0,0 +1,15 @@
+package arrayinput;
+
+public class ArrayPractice {
+
+	public static void main(String[] args) {
+		char [] c = {'a', 'b', 'c', 't'};
+		
+		for(int i = 0; i<c.length; i++) {
+			
+			System.out.print(c[i]+ ", ");
+		}
+
+	}
+
+}
